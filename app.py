bdee2b267d59228dd99c1cae0431a18ebe09a131
Vivek Shet <viveknshet123@gmail.com>
2026-01-21 20:46:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask CI/CD Pipeline with Docker Swarm ____vivek___MAHAGANAPATHI"
+    return "Flask CI/CD Pipeline with Dker Swarm "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
